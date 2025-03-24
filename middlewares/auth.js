c036ec3b0be6ca696b95a227f65e5e2e9d760726
Krishna kant sharma <krishnactive@gmail.com>
2025-03-24 07:04:41 +0530

    check for authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,21 @@
 const {getUser} = require("../service/auth");
 
 
-async function restrictToLoggedinUserOnly(req, res, next){
-    // const userid = req.cookies?.uid;
-    const userUid = req.headers["authorization"];
-
-    if(!userUid) return res.redirect("/login");
-    const token = userUid.split("Bearer ")[1];  //Bearer 2232234jsjkndskln
-    const user = getUser(token);
-    
-    if(!user) return res.redirect("/login");
-
-    req.user = user;
-    next();
-}
-// bearer 
-// https://swagger.io/docs/specification/v3_0/authentication/bearer-authentication/
-async function checkAuth(req, res, next){
-    const userUid = req.headers["authorization"];
-    const token = userUid.split("Bearer ")[1];
-    const user = getUser(token);
+function checkForAuthentication(req, res, next){
+    const authorizationHeaderValue = req.headers["authorization"];
+    req.user = null;
+    if(!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer")){
+        return next();
+    }
+    const token = authorizationHeaderValue.split("Bearer")[1];
+    const user =  getUser(token);
 
     req.user = user;
-    next();
+    return next();
+
 }
+
+
 
 module.exports = {
     restrictToLoggedinUserOnly,
